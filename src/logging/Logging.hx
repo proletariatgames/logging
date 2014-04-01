f05fb77cd134fc3c966e7985fc9f4420223daad5
Luke Petre <luke@proletariat.com>
2014-04-01 18:02:19 -0400

    Use the stack generated for logging.info

--- src/logging/Logging.hx
+++ src/logging/Logging.hx
@@ -18,50 +18,50 @@ class Logging
     static public function info(message:String, arguments:Dynamic=null,
                                 ?stack:Array<StackItem>, ?pos:PosInfos):Void
     {
-        getLogger(null).info(message, arguments);
+        getLogger(null).info(message, arguments, stack, pos);
     }
 
     static public function debug(message:String, arguments:Dynamic=null,
                                  ?stack:Array<StackItem>, ?pos:PosInfos):Void
     {
-        getLogger(null).debug(message, arguments);
+        getLogger(null).debug(message, arguments, stack, pos);
     }
 
     static public function warning(message:String, arguments:Dynamic=null,
                                    ?stack:Array<StackItem>, ?pos:PosInfos):Void
     {
-        getLogger(null).warning(message, arguments);
+        getLogger(null).warning(message, arguments, stack, pos);
     }
 
     static public function warn(message:String, arguments:Dynamic=null,
                                 ?stack:Array<StackItem>, ?pos:PosInfos):Void
     {
-        getLogger(null).warn(message, arguments);
+        getLogger(null).warn(message, arguments, stack, pos);
     }
 
     static public function error(message:String, arguments:Dynamic=null,
                                  ?stack:Array<StackItem>, ?pos:PosInfos):Void
     {
-        getLogger(null).error(message, arguments);
+        getLogger(null).error(message, arguments, stack, pos);
     }
 
     static public function critical(message:String, arguments:Dynamic=null,
                                     ?stack:Array<StackItem>, ?pos:PosInfos):Void
     {
-        getLogger(null).critical(message, arguments);
+        getLogger(null).critical(message, arguments, stack, pos);
     }
 
     static public function fatal(message:String, arguments:Dynamic=null,
                                  ?stack:Array<StackItem>, ?pos:PosInfos):Void
     {
-        getLogger(null).fatal(message, arguments);
+        getLogger(null).fatal(message, arguments, stack, pos);
     }
 
     static public function log(level:Int, message:String, 
                                ?stack:Array<StackItem>, arguments:Dynamic=null,
                                ?pos:PosInfos):Void
     {
-        getLogger(null).log(level, message, arguments);
+        getLogger(null).log(level, message, arguments, stack, pos);
     }
 
     static public function getLogger(name:String):ILogger
